dd1899da58d540b40dcc9da21a2c23a140f35a0a
gkdms04 <haeunselly@gmail.com>
2024-05-01 00:41:35 +0900

    첫 Cargo 사용

---
+++ Hello_Cargo/target/debug/Hello_Cargo.d
@@ -0,0 +1,1 @@
+C:\Users\조하은\Documents\개인\RustStart\Hello_Cargo\target\debug\Hello_Cargo.exe: C:\Users\조하은\Documents\개인\RustStart\Hello_Cargo\src\main.rs
---
+++ Hello_Cargo/target/debug/deps/Hello_Cargo.d
@@ -0,0 +1,5 @@
+C:\Users\조하은\Documents\개인\RustStart\Hello_Cargo\target\debug\deps\Hello_Cargo.exe: src\main.rs
+
+C:\Users\조하은\Documents\개인\RustStart\Hello_Cargo\target\debug\deps\Hello_Cargo.d: src\main.rs
+
+src\main.rs:
